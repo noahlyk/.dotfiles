b6fbd581d7c43aaff8eef955d850961cd9ad3263
fibsussy <noahlykins@gmail.com>
2026-07-11 04:41:06 -0600

    reuse nvim git log spawning multiple window

--- .config/nvim/lua/plugins/fugative.lua
+++ .config/nvim/lua/plugins/fugative.lua
@@ -3,7 +3,37 @@ return {
   cmd = { "G", "Git", "Gvdiffsplit", "GlLog" },
   keys = {
     { "<leader>gs", "<cmd>G<CR>", desc = "Git Status" },
-    { "<leader>gl", "<cmd>Git log --all --decorate --oneline --graph<CR>", desc = "Git Log" },
+    {
+      "<leader>gl",
+      function()
+        local function refresh(buf)
+          vim.bo[buf].modifiable = true
+          vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.fn.systemlist("git log --all --decorate --oneline --graph"))
+          vim.bo[buf].modifiable = false
+        end
+
+        if vim.b[0].is_git_log then
+          refresh(0)
+          return
+        end
+
+        for _, b in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.b[b].is_git_log and vim.api.nvim_buf_is_loaded(b) then
+            for _, w in ipairs(vim.api.nvim_list_wins()) do
+              if vim.api.nvim_win_get_buf(w) == b then
+                vim.api.nvim_set_current_win(w)
+                refresh(b)
+                return
+              end
+            end
+          end
+        end
+
+        vim.cmd("Git log --all --decorate --oneline --graph")
+        vim.b[0].is_git_log = true
+      end,
+      desc = "Git Log",
+    },
     { "<leader>gL", "<cmd>GlLog<CR>", desc = "Git Advanced Log" },
     { "<leader>gf", "<cmd>Git fetch<CR>", desc = "Git Fetch" },
     { "<leader>gp", "<cmd>Git pull<CR>", desc = "Git Pull" },
